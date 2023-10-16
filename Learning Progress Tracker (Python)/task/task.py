4766d936a8977b10e384ce1e91650eff5b18d3fb
Filip Łapkiewicz <80906036+fipachu@users.noreply.github.com>
2023-10-16 13:41:44 +0200

    Rewrite task.py using standard library cmd
    Sadly, the test don't want you to do this. I don't think it's reasonable
    to force cmd into compliance with tests at this point.

--- Learning Progress Tracker (Python)/task/task.py
+++ Learning Progress Tracker (Python)/task/task.py
@@ -1,13 +1,46 @@
-print("Learning progress tracker")
+import cmd
 
-# Interactive UI loop
-while True:
-    command: str = input().lower()
 
-    if command == 'exit':
+class TrackerShell(cmd.Cmd):
+    intro = 'Learning Progress Tracker'
+    prompt = ''
+    # file = None
+
+    def do_exit(self, arg):
         print('Bye!')
-        break
-    elif command == '' or command.isspace():
-        print('No input')
-    else:
-        print('Unknown command!')
+        return True
+
+    def do_EOF(self, arg):
+        return True
+
+    def emptyline(self):
+        print('No input.')
+
+    def default(self, line):
+        print('Error: unknown command!')
+
+    def precmd(self, line):
+        line = line.lower()
+        return line
+
+
+def main():
+    TrackerShell().cmdloop()
+    
+
+if __name__ == "__main__":
+    main()
+
+# print("Learning progress tracker")
+#
+# # Interactive UI loop
+# while True:
+#     command: str = input().lower()
+#
+#     if command == 'exit':
+#         print('Bye!')
+#         break
+#     elif command == '' or command.isspace():
+#         print('No input')
+#     else:
+#         print('Unknown command!')
