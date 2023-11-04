fcd2486b874332bb5d82c522e91f958ae5229913
Filip Łapkiewicz <80906036+fipachu@users.noreply.github.com>
2023-11-04 20:15:24 +0100

    Add example student_data dict

--- Learning Progress Tracker (Python)/task/main.py
+++ Learning Progress Tracker (Python)/task/main.py
@@ -1,3 +1,18 @@
+"""Learning Progress Tracker
+
+
+Example student_data dict with one student inside:
+{
+    752001:
+        {
+            'email': 'c@d.e',
+            'first name': 'Aa',
+            'last name': 'Bb',
+            'points': [0, 1, 2, 3],
+            'submissions': [0, 1, 1, 1]
+        },
+}
+"""
 from tracker import Tracker
 
 if __name__ == "__main__":
